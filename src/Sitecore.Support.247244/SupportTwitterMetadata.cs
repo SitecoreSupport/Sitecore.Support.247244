d73cc8bdc22efa99a874974f20a67cde03649680
DK\EVS <evs@sitecore.com>
2018-12-17 14:26:10 +0200

    inital commit with fix

---
+++ src/Sitecore.Support.247244/SupportTwitterMetadata.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.DependencyInjection;
+using Sitecore.XA.Feature.SiteMetadata.Extensions;
+using Sitecore.XA.Feature.SiteMetadata.Models;
+using Sitecore.XA.Foundation.SitecoreExtensions.Interfaces;
+
+namespace Sitecore.Support.XA.Feature.SiteMetadata.Repositories.TwitterMetadata
+{
+  public class TwitterMetadataRepository : Sitecore.XA.Feature.SiteMetadata.Repositories.TwitterMetadata.TwitterMetadataRepository
+  {
+    protected IPageContext PageContext;
+    public TwitterMetadataRepository()
+    {
+      //fix 247244
+      PageContext = ServiceLocator.ServiceProvider.GetService<IPageContext>();
+    }
+
+    protected override MetaTagModel GetMetatag(string property, ID fieldId)
+    {
+      //fix 247244
+      PageContext = ServiceLocator.ServiceProvider.GetService<IPageContext>();
+      Field field = PageContext.Current.Fields[fieldId];
+      //end of fix 247244
+      if (!string.IsNullOrEmpty(field?.Value))
+      {
+        if (FieldTypeManager.GetField(field) is ImageField)
+        {
+          return new MetaTagModel
+          {
+            Id = fieldId,
+            Property = property,
+            Content = field.GetImageUrl()
+          };
+        }
+        return new MetaTagModel
+        {
+          Id = fieldId,
+          Property = property,
+          Content = field.Value.Replace("\r", " ").Replace("\n", " ").Replace("\t", " ")
+        };
+      }
+      return null;
+    }
+  }
+}
