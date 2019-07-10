794a7a51157aed07c67022b2855e0ad424d52538
sc-WeiHau <whc@sitecore.net>
2019-07-10 19:35:24 +0800

    Now using Configurator instead.

---
+++ src/Sitecore.Support.247244/RegisterSiteMetadataServices.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Support.XA.Feature.SiteMetadata.Repositories.OpenGraphMetadata;
+using Sitecore.Support.XA.Feature.SiteMetadata.Repositories.SeoMetadata;
+using Sitecore.Support.XA.Feature.SiteMetadata.Repositories.TwitterMetadata;
+using Sitecore.XA.Feature.SiteMetadata.Models;
+using Sitecore.XA.Foundation.Mvc.Repositories.Base;
+
+namespace Sitecore.Support
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Sitecore.DependencyInjection;
+    using Sitecore.XA.Foundation.SitecoreExtensions.Services;
+
+    public class RegisterSiteMetadataServices : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IAbstractRepository<OpenGraphRenderingModel>, OpenGraphMetadataRepository>();
+            serviceCollection.AddSingleton<IAbstractRepository<SeoMetadataRenderingModel>, SeoMetadataRepository>();
+            serviceCollection.AddSingleton<IAbstractRepository<TwitterMetadataRenderingModel>, TwitterMetadataRepository>();
+        }
+    }
+}
